8e919468921d4e58ca0c86b72534fd4b740b86a0
mcueto <cueto@live.cl>
2018-01-11 01:05:17 -0300

    Version 0.3.2
    Removed some missing imports

--- rest_framework_apicontrol/models.py
+++ rest_framework_apicontrol/models.py
@@ -2,8 +2,6 @@
 import uuid
 from django.conf import settings
 from django.db import models
-from django.db.models.signals import pre_save
-from .utils import generate_code
 
 
 class Responsible(models.Model):
