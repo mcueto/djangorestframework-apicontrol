75de8d78d5398d59df064fe159ce33d2a43737f0
mcueto <cueto@live.cl>
2018-11-20 01:35:43 -0300

    Update version to 0.6.3

--- rest_framework_apicontrol/__init__.py
+++ rest_framework_apicontrol/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'djangorestframework-apicontrol'
-__version__ = '0.6.2'
+__version__ = '0.6.3'
 __author__ = 'Marcelo Cueto'
 __license__ = 'GPLv3'
 __copyright__ = 'Copyright 2017-2018 Marcelo Cueto'
