299731ed73a42619e08fc8f3b2385862be3452ae
mcueto <cueto@live.cl>
2018-11-19 23:50:21 -0300

    Create generate_random_code utils function

--- rest_framework_apicontrol/utils.py
+++ rest_framework_apicontrol/utils.py
@@ -2,6 +2,10 @@ from random import randrange
 from logging import getLogger
 from . import models
 
+CHARSET = '0123456789ABCDEFGHJKMNPQRSTVWXYZ'
+DEFAULT_LENGTH = 16
+DEFAULT_MAX_TRIES = 32
+
 logger = getLogger(__name__)
 
 
@@ -14,3 +18,21 @@ def get_current_app_from_request(request):
     except Exception as e:
         logger.error(e)
         return None
+
+
+def generate_random_code(length=DEFAULT_LENGTH, max_tries=DEFAULT_MAX_TRIES):
+    code = ""
+    loop_num = 0
+    unique = False
+
+    while not unique:
+        if loop_num < max_tries:
+            new_code = ''
+            for i in range(length):
+                new_code += CHARSET[randrange(0, len(CHARSET))]
+                code = new_code
+                unique = True
+            loop_num += 1
+            return code
+        else:
+            raise ValueError("Couldn't generate a unique code.")
