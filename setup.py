3a86a9c33054ea5210e3af00b19b4af219a64c34
mcueto <cueto@live.cl>
2018-12-19 00:43:19 -0300

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 # from distutils.core import setup
 from setuptools import setup
+from os import path
+
+
+# Read the README.md file content
+def get_long_description(filename='README.md'):
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
 
 setup(
     name='rest_framework_apicontrol',
@@ -7,6 +16,8 @@ setup(
     include_package_data=True,
     version='0.7.6',
     description='Django Rest Framework Library to control Client Apps over APIs',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
     author='Marcelo Cueto',
     author_email='cueto@live.cl',
     url='https://github.com/mcueto/djangorestframework-apicontrol',
